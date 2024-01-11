6ea03a5f45d33b10310464258235fecdd26c21f3
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-11 09:50:11 +0100

    bit util

---
+++ src/utils.rs
@@ -0,0 +1,15 @@
+use halo2_proofs::pasta::Fp;
+
+pub fn u64_to_bits_rev(input: u64, len: usize) -> Vec<Fp> {
+    let mut result = Vec::new();
+    
+    for i in 0..len {
+        // Check the value of the i-th bit
+        let bit_value = (input >> i) & 1;
+        
+        // Push the bit value to the result vector
+        result.push(Fp::from(bit_value));
+    }
+    
+    result
+}
