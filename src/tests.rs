e6bab75408f171081ae899e2708161aebc195a36
Rostyslav Tyshko <tyshko1@gmail.com>
2024-01-11 09:53:51 +0100

    second successful and failing tests

--- src/tests.rs
+++ src/tests.rs
@@ -55,3 +55,54 @@ fn exp_example1_fail() {
     let prover = MockProver::run(k, &circuit, vec![public_input.clone()]).unwrap();
     prover.assert_satisfied();
 }
+
+#[test]
+fn exp_example2() {
+    let k = 5;
+
+    const N: usize = 5;
+
+    let x = Fp::from(2);
+    let result = Fp::from(128);
+    let n = 7;
+
+    let mut n_bits: Vec<_> = u64_to_bits_rev(n, N);
+
+    let circuit = MyCircuit::<Fp, 8>(PhantomData);
+
+    let mut public_input = vec![
+        x,
+        result,
+    ];
+
+    public_input.append(&mut n_bits);
+
+    let prover = MockProver::run(k, &circuit, vec![public_input.clone()]).unwrap();
+    prover.assert_satisfied();
+}
+
+#[test]
+#[should_panic]
+fn exp_example2_fail() {
+    let k = 5;
+
+    const N: usize = 5;
+
+    let x = Fp::from(2);
+    let result = Fp::from(129);
+    let n = 7;
+
+    let mut n_bits: Vec<_> = u64_to_bits_rev(n, N);
+
+    let circuit = MyCircuit::<Fp, 8>(PhantomData);
+
+    let mut public_input = vec![
+        x,
+        result,
+    ];
+
+    public_input.append(&mut n_bits);
+
+    let prover = MockProver::run(k, &circuit, vec![public_input.clone()]).unwrap();
+    prover.assert_satisfied();
+}
